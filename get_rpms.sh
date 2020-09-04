29126a50e6be0f192e8f574dea0fab70175c7602
Nikolai Furmanczak <nikolai.furmanczak@gmail.com>
2020-09-04 16:23:20 +0200

    Delete get_rpms.sh
    no longer needed with Centos 7

--- get_rpms.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-wget -qN --show-progress  http://mirror.centos.org/centos/7/os/x86_64/Packages/compat-libstdc++-33-3.2.3-72.el7.x86_64.rpm
-wget -qN --show-progress http://mirror.centos.org/centos/7/os/x86_64/Packages/compat-libcap1-1.10-7.el7.x86_64.rpm
